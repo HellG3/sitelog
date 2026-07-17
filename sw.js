c3c0f6a82cfd601f8d910a36bd00bb20611cd7d9
HellG3 <emiliogutierrez461@gmail.com>
2026-07-16 18:35:41 -0700

    Add per-lot permit inspection links + Guide tab with WA inspection sequence

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sitelog-v2';
+const CACHE = 'sitelog-v3';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
